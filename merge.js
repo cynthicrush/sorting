202cd6c7934e30153aaa8f30f8888451f8e2bb56
cynthicrush <cynthiazcanada@gmail.com>
2022-09-05 14:43:21 -0500

    code updated

--- merge.js
+++ merge.js
@@ -1,5 +1,38 @@
-function merge() {}
+function merge(array1, array2) {
+    let results = [];
+    let i = 0;
+    let j = 0;
 
-function mergeSort() {}
+    while (i < array1.length && j < array2.length) {
+        if (array2[j] > array1[i]) {
+        results.push(array1[i]);
+        i++;
+        } else {
+        results.push(array2[j]);
+        j++;
+        }
+    }
+
+    while (i < array1.length) {
+        results.push(array1[i]);
+        i++;
+    }
+
+    while (j < array2.length) {
+        results.push(array2[j]);
+        j++;
+    }
+
+    return results;
+}
+
+function mergeSort(array) {
+    if (array.length <= 1) return array;
+    const mid = Math.floor(array.length / 2);
+    const left = mergeSort(array.slice(0, mid));
+    const right = mergeSort(array.slice(mid));
+
+    return merge(left, right);
+}
 
 module.exports = { merge, mergeSort};
